b493bcad5585b3bd44dae3e3ef41fd237079df8f
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-06-22 00:04:53 +0530

    Add files via upload

--- Crud Operation/App.js
+++ Crud Operation/App.js
@@ -1,1 +1,39 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Posts from "./components/posts";
+import TestComponent from "./components/testComponent";
+import Users from "./components/users";
+import { UserProvider } from "./context/UserContext";
+import UserList from "./components/userList";
 
+function App() {
+    return ( <
+        >
+        <
+        BrowserRouter >
+        <
+        UserProvider >
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < TestComponent name = "nagin" / > }
+        /> <
+        Route path = "/post"
+        element = { < Posts / > }
+        /> { /* <Route path="/test" element={<TestComponent />} /> */ } <
+        Route path = "/users"
+        element = { < Users / > }
+        /> <
+        Route path = "/userlist"
+        element = { < UserList / > }
+        /> <
+        /Routes> <
+        /UserProvider> <
+        /BrowserRouter> <
+        />
+    );
+}
+
+export default App;
